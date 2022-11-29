418bb3a00f0ae159005855b17e27e63609856db1
Sherry Xie <sxie04@gmail.com>
2022-11-29 18:05:20 -0500

    Add Reactome 2022 and Tabula Muris

--- docker/buildContainers.sh
+++ docker/buildContainers.sh
@@ -14,5 +14,5 @@ cd ..
 
 cd docker
 
-docker build -f DockerfileAPI -t maayanlab/speedrichr:1.49 .
-docker push maayanlab/speedrichr:1.49
+docker build -f DockerfileAPI -t maayanlab/speedrichr:1.51 .
+docker push maayanlab/speedrichr:1.51
