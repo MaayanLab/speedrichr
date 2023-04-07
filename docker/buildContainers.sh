50ee77074483dae319a245f97417a4d91afc3f97
John Erol Evangelista <je.evangelista04@gmail.com>
2023-04-07 12:08:38 -0400

    published new version

--- docker/buildContainers.sh
+++ docker/buildContainers.sh
@@ -15,5 +15,5 @@ cd ..
 
 cd docker
 
-docker build -f DockerfileAPI -t maayanlab/speedrichr:1.65 .
-docker push maayanlab/speedrichr:1.65
+docker build -f DockerfileAPI -t maayanlab/speedrichr:1.66 .
+docker push maayanlab/speedrichr:1.66
