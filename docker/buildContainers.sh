1b0e22941e6b280c837a3d80e529110aec90d5b0
John Erol Evangelista <je.evangelista04@gmail.com>
2023-10-02 12:26:48 -0400

    removed rummagene-tf

--- docker/buildContainers.sh
+++ docker/buildContainers.sh
@@ -15,5 +15,5 @@ cd ..
 
 cd docker
 
-docker build -f DockerfileAPI -t maayanlab/speedrichr:1.74 .
-docker push maayanlab/speedrichr:1.74
+docker build -f DockerfileAPI -t maayanlab/speedrichr:1.75 .
+docker push maayanlab/speedrichr:1.75
